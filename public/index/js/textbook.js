d25ebab30056781b7dda5f02b42cdabc7fa923fe
Isabelle <isabelle.xu@wustl.edu>
2018-10-07 23:16:14 -0500

    textbook

---
+++ public/index/js/textbook.js
@@ -0,0 +1,16 @@
+/**
+  *
+  * Object class file for a textbook post
+  *
+  */
+
+class Textbook {
+
+  constructor(title, author, ISBN, owner) {
+     this.title = title;
+     this.author = author;
+     this.ISBN = ISBN;
+     this.owner = owner;
+  }
+
+}
