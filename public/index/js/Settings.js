e90aec7dddcbd1e4c27b9e17367620e9837aea06
Isabelle <isabelle.xu@wustl.edu>
2018-11-02 16:30:49 -0500

    update

--- public/index/js/Settings.js
+++ public/index/js/Settings.js
@@ -1,7 +1,8 @@
-$(window).load(function () {
+$(window).load(function() {
   loadCurrentUserInfo();
 
   $("#updateBtn").click(update);
+  $("#updatePassBtn").click(updatePass);
 
 });
 
@@ -24,5 +25,59 @@ function loadCurrentUserInfo() {
 
 let update = () => {
   console.log("user settings update button clicked");
+  var user = firebase.auth().currentUser;
+  let newName = document.getElementById("username").value;
+  let newEmail = document.getElementById("email").value;
+
+  if (newName != "" && newEmail != "") {
+    user.updateProfile({
+      displayName: newName,
+      email: newEmail
+    }).then(function() {
+      // Update successful.
+      location.reload();
+    }).catch(function(error) {
+      // An error happened.
+      alert("Error updating user information. Please try again.");
+    });
+  } else if (newName != "") {
+    user.updateProfile({
+      displayName: newName
+    }).then(function() {
+      // Update successful.
+      location.reload();
+    }).catch(function(error) {
+      // An error happened.
+      alert("Error updating user information. Please try again.");
+    });
+  } else if (newEmail != "") {
+    user.updateProfile({
+      email: newEmail
+    }).then(function() {
+      // Update successful.
+      location.reload();
+    }).catch(function(error) {
+      // An error happened.
+      alert("Error updating user information. Please try again.");
+    });
+  }
 
 };
+
+function updatePass() {
+    var user = firebase.auth().currentUser;
+    let newPass = document.getElementById("newpass").value;
+    let newPassC = document.getElementById("confirm_newpass").value;
+
+    // TODO: Check if old password is correct (for security)
+    if (newPass != newPassC || newPass == "" || newPassC == "") {
+      alert("Passwords don't match!");
+    } else {
+      user.updatePassword(newPass).then(() => {
+        console.log("Updating password successful");
+      }, (error) => {
+        // An error happened.
+        alert("Updating password failed. Please try signing in again.");
+      });
+    }
+}
