2172b41a83419dd28d41142dcd72045f1723abcf
Jeremy Goldstein <jeremy.goldstein@wustl.edu>
2018-12-04 18:15:31 -0600

    bug squashes for mailing and searching

--- public/index/js/Settings.js
+++ public/index/js/Settings.js
@@ -6,9 +6,7 @@ $(window).load(function() {
   //  $("#moderateBtn").click(sendEmail);
   $("#moderateBtn").click(moderatorForm);
 
-
-
-//document.getElementById("moderateBtn").addEventListener("click",moderatorForm);
+  //document.getElementById("moderateBtn").addEventListener("click",moderatorForm);
   document.getElementById("my-file").onchange = function(e) {
     //Get File
     var file = e.target.files[0];
@@ -17,7 +15,9 @@ $(window).load(function() {
       if (user) {
         // User is signed in.
         //Create a Storage Ref
-        var storageRef = firebase.storage().ref(user + '/profilePictures/' + file.name);
+        var storageRef = firebase
+          .storage()
+          .ref(user + "/profilePictures/" + file.name);
         //Upload file
         var task = storageRef.put(file).then(function(snapshot) {
           console.log("Successly inserted image");
@@ -25,54 +25,48 @@ $(window).load(function() {
             console.log("File available at", downloadURL);
             // update user
             var user = firebase.auth().currentUser;
-            user.updateProfile({
-              photoURL: downloadURL
-            }).then(function() {
-              // Update successful.
-              alert("Successfully updated user profile image.");
-            }).catch(function(error) {
-              // An error happened.
-              alert("Error encountered updating user profile image.");
-            });
+            user
+              .updateProfile({
+                photoURL: downloadURL
+              })
+              .then(function() {
+                // Update successful.
+                alert("Successfully updated user profile image.");
+              })
+              .catch(function(error) {
+                // An error happened.
+                alert("Error encountered updating user profile image.");
+              });
             var user = firebase.auth().currentUser;
 
             if (user != null && downloadURL != null) {
               downloadURL = user.photoURL;
-              document.getElementById('profile-pic').src = downloadURL;
+              document.getElementById("profile-pic").src = downloadURL;
               location.reload();
             } else {
               alert("Error uploading profile image.");
             }
             showUserAvatar();
-
           });
         });
-
       } else {
         // No user is signed in.
       }
     });
-
-
   };
 });
-
-
-
-
 
 function showUserAvatar() {
   var user = firebase.auth().currentUser;
 
   if (user != null) {
     if (user.photoURL == null) {
-      document.getElementById('profileImg').src = "../images/placeholder.png";
-      document.getElementById('profile-pic').src = "../images/placeholder.png";
+      document.getElementById("profileImg").src = "../images/placeholder.png";
+      document.getElementById("profile-pic").src = "../images/placeholder.png";
     } else {
-      document.getElementById('profileImg').src = user.photoURL;
-      document.getElementById('profile-pic').src = user.photoURL;
+      document.getElementById("profileImg").src = user.photoURL;
+      document.getElementById("profile-pic").src = user.photoURL;
     }
-
   }
 }
 
@@ -98,54 +92,59 @@ let update = () => {
   let newEmail = document.getElementById("email").value;
 
   if (newName != "" && newEmail != "") {
-    user.updateProfile({
-      displayName: newName,
-    }).then(function() {
-      // Update successful.
-      if (validateEmail(newEmail)) {
-        user.updateEmail(newEmail)
-          .then(() => {
-            clearAlert();
-            $("#alert").append(`<div class="alert alert-success" role="alert">
+    user
+      .updateProfile({
+        displayName: newName
+      })
+      .then(function() {
+        // Update successful.
+        if (validateEmail(newEmail)) {
+          user
+            .updateEmail(newEmail)
+            .then(() => {
+              clearAlert();
+              $("#alert").append(`<div class="alert alert-success" role="alert">
   Succesfully updated user information. Reload the page to see the changes made.
 </div>`);
-            //location.reload();
-          })
-          .catch(function(error) {
-            clearAlert();
-            $("#alert").append(`<div class="alert alert-danger" role="alert">
+              //location.reload();
+            })
+            .catch(function(error) {
+              clearAlert();
+              $("#alert").append(`<div class="alert alert-danger" role="alert">
     Error updating user information. Error message: ${error.message}
     </div>`);
-          });
-      } else {
-        alert("Please enter a valid email address.");
-      }
-
-
-    }).catch(function(error) {
-      // An error happened.
-      clearAlert();
-      $("#alert").append(`<div class="alert alert-danger" role="alert">
+            });
+        } else {
+          alert("Please enter a valid email address.");
+        }
+      })
+      .catch(function(error) {
+        // An error happened.
+        clearAlert();
+        $("#alert").append(`<div class="alert alert-danger" role="alert">
 Error updating user information. Error message: ${error.message}
 </div>`);
-    });
+      });
   } else if (newName != "") {
-    user.updateProfile({
-      displayName: newName
-    }).then(function() {
-      // Update successful.
-      clearAlert();
-      $("#alert").append(`<div class="alert alert-success" role="alert">
+    user
+      .updateProfile({
+        displayName: newName
+      })
+      .then(function() {
+        // Update successful.
+        clearAlert();
+        $("#alert").append(`<div class="alert alert-success" role="alert">
 Succesfully updated user information. Reload the page to see the changes made.
 </div>`);
-      // location.reload();
-    }).catch(function(error) {
-      // An error happened.
-      clearAlert();
-      $("#alert").append(`<div class="alert alert-danger" role="alert">
+        // location.reload();
+      })
+      .catch(function(error) {
+        // An error happened.
+        clearAlert();
+        $("#alert").append(`<div class="alert alert-danger" role="alert">
 Error updating user information. Error message: ${error.message}
 </div>`);
-    });
+      });
   } else if (newEmail != "") {
     if (validateEmail(newEmail)) {
       user.updateEmail(newEmail).then(() => {
@@ -159,59 +158,59 @@ Succesfully updated user information. Reload the page to see the changes made.
       alert("Please enter a valid email address.");
     }
   }
-
 };
 
-function moderatorForm(){
+function moderatorForm() {
   console.log("moderator form");
 
   $("#detailModal").modal("show");
 
+  var sendEmail = function() {
+    adminEmail = "sntegegn13@ole.augie.edu";
+    console.log(adminEmail);
+    var emailVar = document.getElementById("emailVal").value;
+    var nameVar = document.getElementById("nameVal").value;
+    var radios = document.getElementsByName("reason");
+    var classVar = document.getElementById("classVal").value;
 
-    var sendEmail = function() {
-      adminEmail = "sntegegn13@ole.augie.edu"
-      console.log(adminEmail);
-      var emailVar = document.getElementById("emailVal").value;
-      var nameVar = document.getElementById("nameVal").value;
-      var radios = document.getElementsByName("reason");
-      var classVar = document.getElementById("classVal").value;
+    console.log(nameVar);
+    console.log(emailVar);
+    console.log(classVar);
 
-      console.log(nameVar);
-      console.log(emailVar);
-      console.log(classVar);
-
-      for (var i = 0, length = radios.length; i < length; i++)
-      {
-          if (radios[i].checked)
-          {
-            var reasonVar = radios[i].value;
-            console.log(reasonVar);
-          }
+    for (var i = 0, length = radios.length; i < length; i++) {
+      if (radios[i].checked) {
+        var reasonVar = radios[i].value;
+        console.log(reasonVar);
       }
-      console.log(reasonVar);
-      adminEmail = "sntegegn13@ole.augie.edu; isabelle.xu88@gmail.com; jeremy.goldstein@wustl.edu";
-      var link =
-        "mailto:" +
-        adminEmail +
-        "&subject=" +
-        escape("Moderator access") +
-        "&body=" +
-        escape(
-          "Hi! \n\n " +
-          "My name is " + nameVar + ". My email address is "+ emailVar
-          + ". I am " + reasonVar + " for " + classVar + ". I was wondering "+
-          " if I can get a moderator access."
-          + "\n\n Thanks! "
-        );
-      window.location.href = link;
-      console.log(emailVar);
-
-
     }
-    $("#contactBtn").click(sendEmail);
+    console.log(reasonVar);
+    adminEmail =
+      "sntegegn13@ole.augie.edu; isabelle.xu88@gmail.com; jeremy.goldstein@wustl.edu";
+    var link =
+      "mailto:" +
+      adminEmail +
+      "?&subject=" +
+      escape("Moderator access") +
+      "&body=" +
+      escape(
+        "Hi! \n\n " +
+          "My name is " +
+          nameVar +
+          ". My email address is " +
+          emailVar +
+          ". I am " +
+          reasonVar +
+          " for " +
+          classVar +
+          ". I was wondering " +
+          " if I can get a moderator access." +
+          "\n\n Thanks! "
+      );
+    window.location.href = link;
+    console.log(emailVar);
+  };
+  $("#contactBtn").click(sendEmail);
 }
-
-
 
 function updatePass() {
   var user = firebase.auth().currentUser;
@@ -222,12 +221,15 @@ function updatePass() {
   if (newPass != newPassC || newPass == "" || newPassC == "") {
     alert("Passwords don't match!");
   } else {
-    user.updatePassword(newPass).then(() => {
-      console.log("Updating password successful");
-    }, (error) => {
-      // An error happened.
-      alert("Updating password failed. Please try signing in again.");
-    });
+    user.updatePassword(newPass).then(
+      () => {
+        console.log("Updating password successful");
+      },
+      error => {
+        // An error happened.
+        alert("Updating password failed. Please try signing in again.");
+      }
+    );
   }
 }
 
@@ -239,7 +241,6 @@ function validateEmail(email) {
 function clearAlert() {
   var myNode = document.getElementById("alert");
   while (myNode.firstChild) {
-      myNode.removeChild(myNode.firstChild);
+    myNode.removeChild(myNode.firstChild);
   }
-
 }
--- public/index/js/reddit.js
+++ public/index/js/reddit.js
@@ -1,3 +1,14 @@
+var illegalWords = [
+  "piracy",
+  "borrowing",
+  "fraud",
+  "stealing",
+  "theft",
+  "counterfeiting",
+  "cribbing",
+  "lifting",
+  "theft"
+];
 var postKeys = [];
 var clickedListing = "";
 var scores = [];
@@ -78,13 +89,13 @@ function sendEmail(mailList, id) {
   var adminEmail = "";
   for (i = 0; i < mailList.length; i++) {
     console.log(mailList[i]);
-    adminEmail += ";" + mailList[i];
+    adminEmail += mailList[i] + ";";
   }
   var name = getCookie("currentClass");
   var link =
     "mailto:" +
     adminEmail +
-    "&subject=" +
+    "?&subject=" +
     escape("classname-" + name + " postnumber-" + id) +
     "&body=" +
     escape(
@@ -312,6 +323,21 @@ function loadPostListings() {
                 document.getElementById(
                   "postDetailsContent"
                 ).innerHTML = data.val().content;
+                /*
+                for (var word in illegalWords) {
+                  console.log("search:" + word + data.val().content);
+                  if (data.val().content.includes(word)) {
+                    document.getElementById(
+                      "postDetailsContent"
+                    ).innerHTML = data.val().content;
+                  } else {
+                    document.getElementById("postDetailsContent").innerHTML =
+                      "PLAIGARISM DETECTED";
+                    break;
+                  }
+                }
+                */
+
                 if (data.val().fileName != null) {
                   console.log("fileURL is ");
                   console.log(data.val().fileURL);
@@ -692,6 +718,15 @@ function getScores(callback) {
       });
       callback();
     });
+  /*
+  for (let index = 0; index < scores.length; index++) {
+    let rating = rating + scores[index];
+  }
+  let rating = rating / scores.length;
+  rootRef.child("classes/" + currentClassKey).update({
+    rating: rating
+  });
+  */
 }
 
 function getCookie(cname) {
